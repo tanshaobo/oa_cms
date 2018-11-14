d6855670540813e168a7d7242bb22f0b2f67561c
tanshaobo <18235140339@163.com>
2018-11-14 17:38:32 +0800

    新增权限管理、工作中心、登录、人力资源空页面

---
+++ src/page/Auth/Auth.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='Auth'>
+    <h1>Auth Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth'
+}
+</script>
+
+<style lang="stylus">
+#OperateRecord
+  border 1px solid red
+</style>
---
+++ src/page/Personnel/Personnel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='OperateRecord'>
+    <h1>OperateRecord Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OperateRecord'
+}
+</script>
+
+<style lang="stylus">
+#OperateRecord
+  border 1px solid red
+</style>
